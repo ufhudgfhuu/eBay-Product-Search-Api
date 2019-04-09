47a8e44daaec99a0db15d9ebf4e6ceb98e04d261
Jiaying  Luo <jiayingluo@jiayings-macbook-pro.local>
2019-04-09 14:56:10 -0700

    Initial Commit

---
+++ csci571hw9Tests/csci571hw9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  csci571hw9Tests.swift
+//  csci571hw9Tests
+//
+//  Created by Jiaying  Luo on 4/9/19.
+//  Copyright © 2019 Jiaying  Luo. All rights reserved.
+//
+
+import XCTest
+@testable import csci571hw9
+
+class csci571hw9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
